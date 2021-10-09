36fb544ec9263d068f9583071a2732bf4c74a408
OMINDA DILSHAN RESPACT <85678266+omindadelshan@users.noreply.github.com>
2021-10-09 11:35:52 +0530

    Update teletips_set.py

--- teletips_set.py
+++ teletips_set.py
@@ -41,7 +41,8 @@ TELETIPS_MAIN_MENU_BUTTONS = [
 async def start(client, message):
     text = START_TEXT
     reply_markup = InlineKeyboardMarkup(TELETIPS_MAIN_MENU_BUTTONS)
-    await message.reply(
+    await message.reply_video(
+        video= "telegraph",
         text=text,
         reply_markup=reply_markup,
         disable_web_page_preview=True
